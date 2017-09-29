6b186c446206b15d8e45ad7aa7a3f63fc6a60238
wangzeyong <wzy2010416033@163.com>
2017-09-29 11:05:51 +0800

    example change

---
+++ Example/Pods/Target Support Files/PlayableAdsPreviewerDemo/PlayableAdsPreviewerDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PlayableAdsPreviewerDemo : NSObject
+@end
+@implementation PodsDummy_PlayableAdsPreviewerDemo
+@end
--- Example/Pods/Target Support Files/Pods-PlayableAdsPreviewer_Example/Pods-PlayableAdsPreviewer_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PlayableAdsPreviewer_Example/Pods-PlayableAdsPreviewer_Example-frameworks.sh
@@ -103,12 +103,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/HexColors/HexColors.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PlayableAdsPreviewer/PlayableAdsPreviewer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PlayableAdsPreviewerDemo/PlayableAdsPreviewerDemo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TSMessages/TSMessages.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/HexColors/HexColors.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/PlayableAdsPreviewer/PlayableAdsPreviewer.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PlayableAdsPreviewerDemo/PlayableAdsPreviewerDemo.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TSMessages/TSMessages.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then

f8ae1705ac78b3df9eeaf5f7a0675ff1309e959f
wangzeyong <wzy2010416033@163.com>
2017-09-29 12:14:15 +0800

    add UIView Category

---
+++ Example/PlayableAdsPreviewer/PAUIView+CornerRadius.h
@@ -0,0 +1,13 @@
+//
+//  PAUIView+CornerRadius.h
+//  PlayableAdsPreviewer_Example
+//
+//  Created by 王泽永 on 2017/9/29.
+//  Copyright © 2017年 wzy2010416033@163.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CornerRadius)
+@property (nonatomic, assign) IBInspectable CGFloat cornerRadius;
+@end
---
+++ Example/PlayableAdsPreviewer/PAUIView+CornerRadius.m
@@ -0,0 +1,23 @@
+//
+//  PAUIView+CornerRadius.m
+//  PlayableAdsPreviewer_Example
+//
+//  Created by 王泽永 on 2017/9/29.
+//  Copyright © 2017年 wzy2010416033@163.com. All rights reserved.
+//
+
+#import "PAUIView+CornerRadius.h"
+
+@implementation UIView (CornerRadius)
+
+- (void)setCornerRadius:(CGFloat)cornerRadius
+{
+    self.layer.cornerRadius = cornerRadius;
+}
+
+- (CGFloat)cornerRadius
+{
+    return self.layer.cornerRadius;
+}
+
+@end

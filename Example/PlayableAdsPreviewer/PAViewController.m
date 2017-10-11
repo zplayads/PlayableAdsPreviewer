80e58213d5812bfc57561a4bf8d0081d55735a0a
wangzeyong <wzy2010416033@163.com>
2017-10-11 14:39:43 +0800

    add avfoundation framework

--- Example/PlayableAdsPreviewer/PAViewController.m
+++ Example/PlayableAdsPreviewer/PAViewController.m
@@ -10,12 +10,13 @@
 #import "QRCodeReaderViewController.h"
 #import <PlayableAdsPreviewer/PlayableAdsPreviewer.h>
 #import <TSMessages/TSMessage.h>
+#import "PAQRCodeViewController.h"
 
 @interface PAViewController () <QRCodeReaderDelegate>
 
 @property (nonatomic) NSString *appID;
-
 @property (nonatomic) PlayableAdsPreviewer *previewer;
+@property (nonatomic) PAQRCodeViewController *qrVC;
 
 @end
 
@@ -23,85 +24,65 @@
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    [self scanButtonDidPress:nil];
+    
+    [self startScan];
 }
 
-- (void)scanButtonDidPress:(id)sender {
-    if ([QRCodeReader supportsMetadataObjectTypes:@[ AVMetadataObjectTypeQRCode ]]) {
-        static QRCodeReaderViewController *vc = nil;
-        static dispatch_once_t onceToken;
-
-        dispatch_once(&onceToken, ^{
-            QRCodeReader *reader = [QRCodeReader readerWithMetadataObjectTypes:@[ AVMetadataObjectTypeQRCode ]];
-            vc = [QRCodeReaderViewController readerWithCancelButtonTitle:@"Cancel"
-                                                              codeReader:reader
-                                                     startScanningAtLoad:YES
-                                                  showSwitchCameraButton:YES
-                                                         showTorchButton:YES];
-            vc.modalPresentationStyle = UIModalPresentationFormSheet;
-        });
-        vc.delegate = self;
-
-        [vc setCompletionWithBlock:^(NSString *resultAsString) {
-            NSLog(@"Completion with result: %@", resultAsString);
-        }];
-
-        [self presentViewController:vc animated:YES completion:NULL];
-    } else {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Error"
-                                                        message:@"Reader not supported by the current device"
-                                                       delegate:nil
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
-
-        [alert show];
-    }
+- (void)startScan{
+    self.qrVC = [[PAQRCodeViewController alloc] initWithCompletion:^(BOOL succeeded, NSString *result) {
+        if (succeeded) {
+            self.appID = result;
+            [TSMessage showNotificationInViewController:self
+                                                  title:@"Successed"
+                                               subtitle:result
+                                                  image:nil
+                                                   type:TSMessageNotificationTypeSuccess
+                                           duration:TSMessageNotificationDurationAutomatic
+                                               callback:nil
+                                            buttonTitle:nil
+                                         buttonCallback:nil
+                                             atPosition:TSMessageNotificationPositionTop
+                                   canBeDismissedByUser:YES];
+            [self requestAd];
+        } else {
+            [TSMessage showNotificationInViewController:self
+                                                  title:@"Error"
+                                               subtitle:result
+                                                  image:nil
+                                                   type:TSMessageNotificationTypeError
+                                           duration:TSMessageNotificationDurationAutomatic
+                                               callback:nil
+                                            buttonTitle:nil
+                                         buttonCallback:nil
+                                             atPosition:TSMessageNotificationPositionTop
+                                   canBeDismissedByUser:YES];
+        }
+    }];
+    [self presentViewController:self.qrVC animated:YES completion:NULL];
 }
 
 - (void)requestAd {
     [self.previewer presentFromRootViewController:self
         withAdID:self.appID
         success:^{
+            [self.qrVC stop];
         }
         dismiss:^{
+            [self startScan];
         }
         failure:^(NSError *_Nonnull error) {
-            NSLog(@"%@", error);
+            [TSMessage showNotificationInViewController:self
+                                                  title:@"Error"
+                                               subtitle:nil
+                                                  image:nil
+                                                   type:TSMessageNotificationTypeError
+                                           duration:TSMessageNotificationDurationAutomatic
+                                               callback:nil
+                                            buttonTitle:nil
+                                         buttonCallback:nil
+                                             atPosition:TSMessageNotificationPositionTop
+                                   canBeDismissedByUser:YES];
         }];
-}
-
-#pragma mark - QRCodeReader Delegate Methods
-- (void)reader:(QRCodeReaderViewController *)reader didScanResult:(NSString *)result {
-    self.appID = result;
-
-    [reader stopScanning];
-    [reader dismissViewControllerAnimated:YES
-                               completion:^{
-                                   [TSMessage showNotificationInViewController:self
-                                                                         title:@"Successed"
-                                                                      subtitle:result
-                                                                         image:nil
-                                                                          type:TSMessageNotificationTypeSuccess
-                                                                      duration:TSMessageNotificationDurationAutomatic
-                                                                      callback:nil
-                                                                   buttonTitle:nil
-                                                                buttonCallback:nil
-                                                                    atPosition:TSMessageNotificationPositionTop
-                                                          canBeDismissedByUser:YES];
-                                   [self requestAd];
-                               }];
-}
-
-- (void)readerDidCancel:(QRCodeReaderViewController *)reader {
-    [reader dismissViewControllerAnimated:YES completion:NULL];
-}
-
-#pragma mark - lazyLoad
-- (PlayableAdsPreviewer *)previewer {
-    if (!_previewer) {
-        _previewer = [[PlayableAdsPreviewer alloc] init];
-    }
-    return _previewer;
 }
 
 @end
